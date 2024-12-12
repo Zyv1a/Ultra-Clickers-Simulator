8c333ef05279115f827405d8ab1711eb5c91b8ff
Zyv1a <110346397+Zyv1a@users.noreply.github.com>
2024-12-12 18:00:30 +0000

    Rework and test

--- index.js
+++ index.js
@@ -1,26 +1,17 @@
-// Importer les modules nécessaires
 const http = require('http');
 const WebSocket = require('ws');  // Importer la bibliothèque WebSocket
 
-// Définir le port
-const PORT = process.env.PORT || 3001;  // Utiliser le port défini par Vercel, sinon 3000
+const PORT = process.env.PORT || 3001;  // Utiliser le port défini par Vercel, sinon 3001
 
-// Créer le serveur HTTP
 const server = http.createServer((req, res) => {
     res.end('Le serveur backend est opérationnel !');
 });
 
-// Créer un serveur WebSocket
 const wss = new WebSocket.Server({ server });
 
-// Gérer les connexions WebSocket
 wss.on('connection', (ws) => {
     console.log('Nouvelle connexion WebSocket établie');
 
-    // Envoyer un message au client lorsque la connexion est établie
-    ws.send('Bienvenue sur le serveur WebSocket !');
-
-    // Simuler des données de classement
     const leaderboardData = {
         clicks: [
             { playerName: 'Player1', score: 1500 },
@@ -39,24 +30,19 @@ wss.on('connection', (ws) => {
         ]
     };
 
-    // Envoyer les données de classement toutes les 5 secondes
     setInterval(() => {
         ws.send(JSON.stringify(leaderboardData));  // Envoyer les données en JSON
     }, 5000);
 
-    // Gérer les messages reçus du client
     ws.on('message', (message) => {
         console.log('Message reçu du client:', message);
-        // Vous pouvez traiter les messages reçus ici
     });
 
-    // Gérer la fermeture de la connexion WebSocket
     ws.on('close', () => {
         console.log('Connexion WebSocket fermée');
     });
 });
 
-// Lancer le serveur sur le port défini (ou utiliser le port par défaut 3000)
 server.listen(PORT, () => {
     console.log(`Serveur actif sur le port ${PORT}`);
 });
